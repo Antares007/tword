7191552c2df59d785ce3fc67f8f038f1fcd80b33
Archil Bolkvadze <a.bolkvadze@gmail.com>
2026-02-21 19:18:26 +0400

    typed ab.c

--- ab.c
+++ ab.c
@@ -1,192 +1,219 @@
-#define N(argo) void argo(struct Sac gram, struct Sac o, struct Ray *s, const char*in, int pos)
-#define S static N
-#define StepCon gram, o, s, in, pos
-enum { Pink, Blue, Green, Yellow, Red };
-struct Ray;
-typedef struct Sac { N((**c)) ; } Sac;
-typedef struct Ray { Sac gram; struct Ray *upper; char color; } Ray;
-typedef N((*Step));
+typedef struct α α;
+typedef struct γ γ;
+typedef struct τε τε;
+typedef struct ταγ ταγ;
+typedef struct τγ τγ;
+typedef struct τγγ τγγ;
+struct γ { void (*step)(int *, τε, τγγ, ταγ, τγγ); };
+struct α { void (*step)(int *, τε, τγ, τε, γ); };
+struct τε { void (*step)(int *); };
+struct τγ { void (*step)(int *, γ); };
+struct τγγ { void (*step)(int *, γ, γ); };
+struct ταγ { void (*step)(int *, α, γ); };
 
-S(not) { o.c[0](StepCon); }
-S(and) { o.c[1](StepCon); }
-S(or ) { o.c[2](StepCon); }
+#define EAX 0
+#define EBX 1
+#define ESP 2
+#define EBP 3
 
-S(Sa);
-S(b) { (in[pos] == 'b' ? and : or)(gram, o, s, in, pos + 1); }
-S(a) { (in[pos] == 'a' ? and : or)(gram, o, s, in, pos + 1); }
-S(unit18_2) { static Step v[] = {not,      a};      gram = (Sac){v}, o.c[2](StepCon); }
-S(unit18_1) { static Step v[] = {unit18_2, Sa};     gram = (Sac){v}, o.c[3](StepCon); }
-S(unit18  ) { static Step v[] = {unit18_1, not};    gram = (Sac){v}, o.c[1](StepCon); }
-S(Sa_1    ) { static Step v[] = {unit18,   b};      gram = (Sac){v}, o.c[2](StepCon); }
-S(Sa      ) { static Step v[] = {Sa_1,     unit18}; gram = (Sac){v}, o.c[1](StepCon); }
-S(expression);
-S(gram);
-S((*srart));
-extern int printf (const char *__restrict __format, ...);
-S(accept) { printf("true in: %s pos: %d\n", in, pos); }
+void b(int *o, τε ascend, τγ and, τε or, γ unit2) {
+  o[EAX] = 0, o[EBX] = 1, and.step(o, unit2);
+}
+void a(int *o, τε ascend, τγ and, τε or, γ unit2) {
+  int prev = o[EAX];
+  int current = o[EBX];
+  int next = prev + current;
+  o[EAX] = current;
+  o[EBX] = next;
+  if (next < 144) and.step(o, unit2);
+  else ascend.step(o);
+}
+void S0(int *o, τε op0, τγγ op1, ταγ op2, τγγ op3);
+void End(int *o, τε op0, τγγ op1, ταγ op2, τγγ op3) { op0.step(o); }
+void S12(int *o, τε op0, τγγ op1, ταγ op2, τγγ op3) {
+  op2.step(o, (α){a}, (γ){End});
+}
+void S11(int *o, τε op0, τγγ op1, ταγ op2, τγγ op3) {
+  op3.step(o, (γ){S0}, (γ){S12});
+}
+void S1(int *o, τε op0, τγγ op1, ταγ op2, τγγ op3) {
+  op1.step(o, (γ){End}, (γ){S11});
+}
+void S01(int *o, τε op0, τγγ op1, ταγ op2, τγγ op3) {
+  op2.step(o, (α){b}, (γ){S1});
+}
+void S0(int *o, τε op0, τγγ op1, ταγ op2, τγγ op3) {
+  op1.step(o, (γ){S1}, (γ){S01});
+}
+extern int printf(const char *__restrict __format, ...);
+void accept(int *o, τε op0, τγγ op1, ταγ op2, τγγ op3) {
+  int prev = o[EBX];
+  int current = o[EAX];
+  printf("%d x %d = %d\n", prev, current, prev * current);
+}
+
+typedef γ op1_t;
+typedef α op2_t;
+typedef γ op3_t;
+#define Γ(name)                                                               \
+  void name(int *o, τε op0, τγγ op1, ταγ op2, τγγ op3)
+#define Δ(unit2, opcode, unit1, name)                                         \
+    Γ(name) { op##opcode.step(o, (op##opcode##_t){unit1}, (γ){unit2}); }
+#include"grammar.h"
+
+D(S, 2, b,         A)D(A,
+     3, S, 2, a, End)
+
+void Pink_3(int *o, γ unit1, γ unit2);
 int main() {
-  gram((Sac){(Step[]){accept, Sa}},             (Sac){0}, 0, "baaaaaa",  0);
-  gram((Sac){(Step[]){accept, expression}},     (Sac){0}, 0, "(1+2)*3", 0);
-} 
-S(lex_1     ) { Step n = (in[pos++] == '1' ? and : or); n(StepCon); }
-S(lex_2     ) { Step n = (in[pos++] == '2' ? and : or); n(StepCon); }
-S(lex_3     ) { Step n = (in[pos++] == '3' ? and : or); n(StepCon); }
-S(lex_op    ) { Step n = (in[pos++] == '(' ? and : or); n(StepCon); }
-S(lex_cp    ) { Step n = (in[pos++] == ')' ? and : or); n(StepCon); }
-S(lex_plus  ) { Step n = (in[pos++] == '+' ? and : or); n(StepCon); }
-S(lex_minus ) { Step n = (in[pos++] == '-' ? and : or); n(StepCon); }
-S(lex_bang  ) { Step n = (in[pos++] == '!' ? and : or); n(StepCon); }
-S(lex_mul   ) { Step n = (in[pos++] == '*' ? and : or); n(StepCon); }
-S(lex_div   ) { Step n = (in[pos++] == '/' ? and : or); n(StepCon); }
-S(lex_semi  ) { Step n = (in[pos++] == ';' ? and : or); n(StepCon); }
-S((*_[])) = { 
-  ['1'] = lex_1, ['2'] = lex_2,
-  ['3'] = lex_3, ['('] = lex_op,
-  [')'] = lex_cp, ['+'] = lex_plus,
-  ['-'] = lex_minus, ['!'] = lex_bang,
-  ['*'] = lex_mul, ['/'] = lex_div,
-  [';'] = lex_semi,
-};
-#include "programmer.h"
-#define Δ(next, opcode, unit, name) S(name) { gram = (Sac){(Step[]){next, unit}}, o.c[opcode](StepCon); }
-D(constant,       2, _['1'], A)D(A,
-                  2, _['2'], A)D(A,
-                  2, _['3'], not);
-S(expression);
-D(primary,        3, constant, A)D(A,
-                  2, _['('], 3, expression, 2, _[')'], not);
-D(unary,          3, primary, A)D(A,
-                  2, _['-'], 3, unary, A)D(A,
-                  2, _['!'], 3, unary, not)
-D(multiplicative, 3, unary, A)D(A,
-                  3, multiplicative, 2, _['*'], 3, unary, A)D(A,
-                  3, multiplicative, 2, _['/'], 3, unary, not)
-D(additive,       3, multiplicative, A)D(A,
-                  3, additive, 2, _['+'], 3, multiplicative, A)D(A,
-                  3, additive, 2, _['-'], 3, multiplicative, not)
-D(expression,     3, additive, not)
-D(expression_stm, 3, expression, 2, _[';'], not)
-// below is choice machine algorith
-// which runs grammar and runs
-// actions too.
-// lets imagine root/grammar
-// namespace which can evolution
-// in time as one bounded set and
-// actions namespace as
-// crown/growing branches and
-// furits
-S(reTurn) { return (void)0; }
-S((*Red_descend_steps[]));
-S((*Yellow_descend_steps[]));
-S((*Red_walk_steps[]));
-S((*Yellow_walk_steps[]));
-S((*Green_walk_steps[]));
-S((*Green_NotAndOr_steps[]));
-S((*Blue_walk_steps[]));
-S((*Blue_NotAndOr_steps[]));
-S(Blue_not) {
-  Step c = s->gram.c[0];
-  if      (s->color == Yellow)        o = (Sac){Blue_walk_steps},  s = s->upper, c(StepCon);
-  else if (s->color == Blue)          o = (Sac){Blue_walk_steps},  s = s->upper, c(StepCon);
-  else                                o = (Sac){Green_walk_steps}, s = s->upper, c(StepCon);
+  int o[1023];
+  o[EBP] = o[ESP] = sizeof(o) / sizeof(*o);
+  Pink_3(o, (γ){S0}, (γ){accept});
+  Pink_3(o, (γ){S}, (γ){accept});
 }
-S(Blue_and) { o = (Sac){Blue_walk_steps}, gram.c[0](StepCon); }
-S((*Blue_NotAndOr_steps[])) = {
-  Blue_not,
-  Blue_and,
-  reTurn
-};
-S(Blue_dot) {
-  Blue_not(StepCon);
-  if      (s->color == Yellow) return;
-  o = (Sac){Yellow_descend_steps}, s->gram.c[1](StepCon);
+void Red_0(int *o);
+void Red_01(int *o, γ a, γ b);
+void Red_1(int *o, γ a, γ b);
+void Red_3(int *o, γ a, γ b);
+void Yellow_1(int *o, γ a, γ b);
+void Yellow_3(int *o, γ a, γ b);
+void Green_0(int *o);
+void Green_01(int *o, γ a, γ b);
+void Green_2(int *o, α a, γ b);
+void Green_3(int *o, γ a, γ b);
+void Blue_0(int *o);
+void Blue_01(int *o, γ a, γ b);
+void Blue_2(int *o, α a, γ b);
+void Blue_3(int *o, γ a, γ b);
+
+enum { Pink, Blue, Green, Yellow, Red };
+void anchor(int *o, τε op0, τγγ op1, ταγ op2, τγγ op3) {}
+void ret_t(int *o) {}
+int *branch(int *o, γ a, γ b, int color) {
+  int i = o[ESP] -= 4;
+  o[i + 3] = a.step - anchor;
+  o[i + 2] = b.step - anchor;
+  o[i + 1] = o[EBP];
+  o[i + 0] = Pink;
+  o[EBP] = i;
+  return o;
 }
-S(Blue_walk_dispatch) {
-  o = (Sac){Blue_NotAndOr_steps}, gram.c[1](StepCon);
+γ get_a(int *o, int iptr) { return (γ){anchor + o[iptr + 3]}; }
+γ get_b(int *o, int iptr) { return (γ){anchor + o[iptr + 2]}; }
+int get_upper(int *o, int iptr) { return o[iptr + 1]; }
+int get_color(int *o, int iptr) { return o[iptr + 0]; }
+void Pink_3(int *o, γ a, γ b) {
+  a.step(branch(o, a, b, Pink), (τε){ret_t}, (τγγ){Red_1}, (ταγ){Green_2},
+         (τγγ){Red_3});
 }
-S(Blue_branch_Red_descend) {
-  o = (Sac){Red_descend_steps}, s = &(Ray){gram, s, Blue}, gram.c[1](StepCon);
+void Red_1(int *o, γ a, γ b) {
+  int esp = o[ESP]; int ebp = o[EBP];
+  b.step(o, (τε){Red_0}, (τγγ){Red_01}, (ταγ){Green_2}, (τγγ){Red_3});
+  o[ESP] = esp; o[EBP] = ebp;
+  a.step(o, (τε){ret_t}, (τγγ){Red_1}, (ταγ){Green_2}, (τγγ){Red_3});
 }
-S((*Blue_walk_steps[])) = {
-    Blue_dot,
-    Blue_dot,
-    Blue_walk_dispatch,
-    Blue_branch_Red_descend,
-};
-S(Green_not) {
-  Step c = s->gram.c[0];
-  if (s->color == Blue) o = (Sac){Blue_walk_steps},  s = s->upper, c(StepCon);
-  else                  o = (Sac){Green_walk_steps}, s = s->upper, c(StepCon);
+void Red_01(int *o, γ a, γ b) { Red_0(o); }
+int *move_up(int *o) { return o[EBP] = get_upper(o, o[EBP]), o; }
+void Red_0(int *o) {
+  if (get_color(o, o[EBP]) == Red)
+    get_b(o, o[EBP]).step(move_up(o), (τε){Red_0}, (τγγ){Red_01},
+                          (ταγ){Green_2}, (τγγ){Red_3});
+  else if (get_color(o, o[EBP]) == Blue)
+    get_b(o, o[EBP]).step(move_up(o), (τε){Blue_0}, (τγγ){Blue_01},
+                          (ταγ){Blue_2}, (τγγ){Blue_3});
+  else
+    get_b(o, o[EBP]).step(move_up(o), (τε){Green_0}, (τγγ){Green_01},
+                          (ταγ){Green_2}, (τγγ){Green_3});
 }
-S(Green_and) { o = (Sac){Green_walk_steps}, gram.c[0](StepCon); }
-S((*Green_NotAndOr_steps[])) = {
-  Green_not,
-  Green_and,
-  reTurn
-};
-S(Green_dot) {
-  Green_not(StepCon);
-  o = (Sac){Yellow_descend_steps}, s->gram.c[1](StepCon);
-}
-S(Green_walk_dispatch) {
-  o = (Sac){Green_NotAndOr_steps}, gram.c[1](StepCon);
-}
-S(Green_branch_Red_descend) {
-  o = (Sac){Red_descend_steps}, s = &(Ray){gram, s, Green}, gram.c[1](StepCon);
-}
-S((*Green_walk_steps[])) = {
-    Green_dot,
-    Green_dot,
-    Green_walk_dispatch,
-    Green_branch_Red_descend,
-};
-S(Yellow_grow) {
-  Ray *d = s;
-  for (; d->color == Yellow; d = d->upper) if (d->gram.c[1] == gram.c[1]) return;
-  if (d->gram.c[1] == gram.c[1]) o = (Sac){Blue_walk_steps}, gram.c[0](StepCon);
-  else o = (Sac){Yellow_descend_steps}, s = &(Ray){gram, s, Yellow}, gram.c[1](StepCon);
-}
-S((*Yellow_walk_steps[])) = {
-    reTurn,
-    reTurn,
-    reTurn,
-    Yellow_grow,
-};
-S(Red_dot) {
-  Step c = s->gram.c[0];
-  if (s->color == Red)       o = (Sac){Red_walk_steps},   s = s->upper, c(StepCon);
-  else if (s->color == Blue) o = (Sac){Blue_walk_steps},  s = s->upper, c(StepCon);
-  else                       o = (Sac){Green_walk_steps}, s = s->upper, c(StepCon);
-}
-S(Red_branch_Red_descend) {
-  Ray *d = s;
+void Red_3(int *o, γ a, γ b) {
+  int d = o[EBP];
   while (1) {
-    if (d->gram.c[1] == gram.c[1]) return;
-    if (d->color != Red)
-      return o = (Sac){Red_descend_steps}, s = &(Ray){gram, s, Red}, gram.c[1](StepCon);
-    d = d->upper;
+    if (get_a(o, d).step == a.step)
+      return ret_t(o);
+    if (get_color(o, d) != Red)
+      return a.step(branch(o, a, b, Red), (τε){ret_t}, (τγγ){Red_1},
+                    (ταγ){Green_2}, (τγγ){Red_3});
+    d = get_upper(o, d);
   }
 }
-S((*Red_walk_steps[])) = {
-    Red_dot,
-    Red_dot,
-    Green_walk_dispatch,
-    Red_branch_Red_descend,
-};
-S(Yellow_descend1) {
-  o = (Sac){Yellow_walk_steps},   gram.c[0](StepCon);
-  o = (Sac){Yellow_descend_steps},gram.c[1](StepCon);
+void ret_tgg(int *o, γ a, γ b) {}
+void ret_tag(int *o, α a, γ b) {}
+void Yellow_1(int *o, γ a, γ b) {
+  int esp = o[ESP];
+  int ebp = o[EBP];
+  b.step(o, (τε){ret_t}, (τγγ){ret_tgg}, (ταγ){ret_tag}, (τγγ){Yellow_3});
+  o[ESP] = esp;
+  o[EBP] = ebp;
+  a.step(o, (τε){ret_t}, (τγγ){Yellow_1}, (ταγ){ret_tag}, (τγγ){Yellow_3});
 }
-S((*Yellow_descend_steps[])) = {
-  reTurn,
-  Yellow_descend1
-};
-S(Red_descend1) {
-  o = (Sac){Red_walk_steps},    gram.c[0](StepCon);
-  o = (Sac){Red_descend_steps}, gram.c[1](StepCon);
+void Yellow_3(int *o, γ a, γ b) {
+  int d = o[EBP];
+  for (; get_color(o, d) == Yellow; d = get_upper(o, d))
+    if (get_a(o, d).step == a.step)
+      return ret_t(o);
+  if (get_a(o, d).step == a.step)
+    b.step(o, (τε){Blue_0}, (τγγ){Blue_01}, (ταγ){Blue_2}, (τγγ){Blue_3});
+  else
+    a.step(branch(o, a, b, Yellow), (τε){ret_t}, (τγγ){Yellow_1},
+           (ταγ){ret_tag}, (τγγ){Yellow_3});
 }
-S((*Red_descend_steps[])) = {
-  reTurn,
-  Red_descend1
-};
-S(gram) { o = (Sac){Red_descend_steps}, s = &(Ray){gram, s, Pink}, gram.c[1](StepCon); }
+void Green_not(int *o) {
+  if (get_color(o, o[EBP]) == Blue)
+    get_b(o, o[EBP]).step(move_up(o), (τε){Blue_0}, (τγγ){Blue_01},
+                          (ταγ){Blue_2}, (τγγ){Blue_3});
+  else
+    get_b(o, o[EBP]).step(move_up(o), (τε){Green_0}, (τγγ){Green_01},
+                          (ταγ){Green_2}, (τγγ){Green_3});
+}
+void Green_01(int *o, γ a, γ b) { Green_0(o); }
+void Green_and(int *o, γ b) {
+  b.step(o, (τε){Green_0}, (τγγ){Green_01}, (ταγ){Green_2}, (τγγ){Green_3});
+}
+void Green_0(int *o) {
+  int esp = o[ESP];
+  int ebp = o[EBP];
+  Green_not(o);
+  o[ESP] = esp;
+  o[EBP] = ebp;
+  get_a(o, o[EBP]).step(o, (τε){ret_t}, (τγγ){Yellow_1}, (ταγ){ret_tag},
+                        (τγγ){Yellow_3});
+}
+void ret_v(int *o) {}
+
+void Green_2(int *o, α a, γ b) {
+  a.step(o, (τε){Green_not}, (τγ){Green_and}, (τε){ret_v}, b);
+}
+void Green_3(int *o, γ a, γ b) {
+  a.step(branch(o, a, b, Green), (τε){ret_t}, (τγγ){Red_1}, (ταγ){Green_2},
+         (τγγ){Red_3});
+}
+void Blue_not(int *o) {
+  if (get_color(o, o[EBP]) == Yellow || get_color(o, o[EBP]) == Blue)
+    get_b(o, o[EBP]).step(move_up(o), (τε){Blue_0}, (τγγ){Blue_01},
+                          (ταγ){Blue_2}, (τγγ){Blue_3});
+  else
+    get_b(o, o[EBP]).step(move_up(o), (τε){Green_0}, (τγγ){Green_01},
+                          (ταγ){Green_2}, (τγγ){Green_3});
+}
+void Blue_and(int *o, γ b) {
+  b.step(o, (τε){Blue_0}, (τγγ){Blue_01}, (ταγ){Blue_2}, (τγγ){Blue_3});
+}
+void Blue_01(int *o, γ a, γ b) { Blue_0(o); }
+void Blue_0(int *o) {
+  int esp = o[ESP];
+  int ebp = o[EBP];
+  Blue_not(o);
+  if (get_color(o, o[EBP]) == Yellow)
+    return ret_v(o);
+  o[ESP] = esp;
+  o[EBP] = ebp;
+  get_a(o, o[EBP]).step(o, (τε){ret_t}, (τγγ){Yellow_1}, (ταγ){ret_tag},
+                        (τγγ){Yellow_3});
+}
+void Blue_2(int *o, α a, γ b) {
+  a.step(o, (τε){Blue_not}, (τγ){Blue_and}, (τε){ret_v}, b);
+}
+void Blue_3(int *o, γ a, γ b) {
+  a.step(branch(o, a, b, Blue), (τε){ret_t}, (τγγ){Red_1}, (ταγ){Green_2},
+         (τγγ){Red_3});
+}
